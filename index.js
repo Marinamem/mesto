b4074d519d46ce3c9f22a32abee74b7001349938
Marina Kleshcheva <marisabelka200000@yandex.ru>
2023-06-04 15:52:03 +0300

    feat: верстка сайта и js для popup

---
+++ index.js
@@ -0,0 +1,34 @@
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector(".popup__name-info"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector(".popup__job-info"); // Воспользуйтесь инструментом .querySelector()
+let editButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".popup__close-button");
+let saveButton = document.querySelector(".popup__save-button");
+let popup = document.querySelector(".popup");
+let profileName = document.querySelector(".profile__info-name");
+let profileJob = document.querySelector(".profile__info-job");
+
+const openPopup = () => {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+};
+
+const closePopup = () => {
+  popup.classList.remove("popup_opened");
+};
+
+editButton.addEventListener("click", openPopup);
+saveButton.addEventListener("click", closePopup);
+closeButton.addEventListener("click", closePopup);
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+
+function handleFormSubmit(evt) {
+  evt.preventDefault(); //
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+formElement.addEventListener("submit", handleFormSubmit);
